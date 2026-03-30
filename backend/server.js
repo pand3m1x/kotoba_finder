2eecff9b49a5f94624f90f7e2a0a7431360f1397
TheRedQueen <pand3m1x@gmail.com>
2026-03-30 13:22:47 -0400

    °❀.ೃ  added auth and connect and server code, adding User Schema next °❀.ೃ

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import 'dotenv/config'
+import './config/connection.js'
+import express from 'express'
+import userRoutes from './routes/userRoutes.js'
+// import postRoutes from './routes/postRoutes.js'
+
+const app = express()
+import cors from 'cors'
+
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(cors()) //['http://localhost:5173'] < This is for anything that communicates with the backend, telling it, 'it's okay' 
+
+// app.use('/api/posts', postRoutes)
+// app.use('/api/posts', postRoutes)
+app.use('/api/users', userRoutes)
+
+app.get('/', (req,res) => {
+  res.send('hello world!')
+})
+
+app.listen(port,() => console.log(`Mic Check, Mic Check: http://localhost:${port}`))
---
+++ backend/utils/auth.js
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken'
+
+const secret = process.env.JWT_SECRET
+
+export async function authMiddleware( req, res, next ) {
+console.log("HEADERS:", req.headers);
+  try{
+
+  let token = req.headers.authorization
+
+  // check if there is a token
+  if (!token) {
+    return res.status(403).json({ message: 'no token provided' })
+  }
+
+  // remove the 'Bearer' from the token
+  token = token.split(' ').pop().trim()
+
+  // verify the token
+  const { data } = jwt.verify(token, secret)
+
+  // pass the payload
+  req.user = data
+
+  // move onto the route
+  next()
+  
+} catch(err) {
+
+  console.log(err.message)
+  res.status(400).json({ message: err.message })
+
+}};
