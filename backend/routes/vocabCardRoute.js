4317697d81544d2ed4864ad6dd9e10f05af7f3c9
TheRedQueen <pand3m1x@gmail.com>
2026-03-31 14:09:06 -0400

    °❀.ೃ  changed vocab route back to test routs °❀.ೃ

--- backend/routes/vocabCardRoute.js
+++ backend/routes/vocabCardRoute.js
@@ -4,46 +4,21 @@ const router = express.Router();
 
 // import item from '../models/items.js'
 import User from '../models/user.js';
-import { authMiddleware } from '../utils/auth.js'
+// import { authMiddleware } from '../utils/auth.js'
 
-router.use(authMiddleware)
-// updates unlocked vocab as user progresses
-router.post('/', async (req,res) =>{
+// router.use(authMiddleware)
+// // updates unlocked vocab as user progresses , this is for full version with use auth
+// router.post('/', async (req,res) =>{
 
-  console.log("Adding vocab to user");
-  try{
-
-    console.log(req.user)
-    const { _id } = req.user; // coming in from auth middlerware
-    const { itemId } = req.body; // coming from postman req
-    
-    const user = await User.findById(_id);
-    // console.log(user,userId)
-
-    user.vocab.push({ item: itemId })
-
-    await user.save()
-    
-    console.log("success!", itemId)
-    return res.json(user);
-
-  } catch(err) {
-
-    console.log(err.message, "vocab no added")
-    return res.status(500).json({ message: "failed to add vocab"})
-
-  }
-});
-
-//test rout /vocab/test
-// router.post('/test', async (req,res) =>{
-
-//   console.log("test route hit")
+//   console.log("Adding vocab to user");
 //   try{
 
-//     const { userId, itemId } = req.body;
-
-//     const user = await User.findById(userId);
+//     console.log(req.user)
+//     const { _id } = req.user; // coming in from auth middlerware
+//     const { itemId } = req.body; // coming from postman req
+    
+//     const user = await User.findById(_id);
+//     // console.log(user,userId)
 
 //     user.vocab.push({ item: itemId })
 
@@ -60,30 +35,37 @@ router.post('/', async (req,res) =>{
 //   }
 // });
 
-// gets all vocab for user
-router.get('/:id', async (req,res) => {
+// test rout /vocab/test
+router.post('/test', async (req,res) =>{
 
+  console.log("test route hit")
   try{
 
-    const user = await User.findById(req.user._id)
-                           .populate('vocab.item');
+    const { userId, itemId } = req.body;
 
-    console.log("Grabbing user's learned vocab")
-    return res.json(user.vocab)
+    const user = await User.findById(userId);
+
+    user.vocab.push({ item: itemId })
+
+    await user.save()
+    
+    console.log("success!", itemId)
+    return res.json(user);
 
   } catch(err) {
 
-    console.log(err.message, "vocab not found")
-    return res.status(500).json({ message: "failed to find vocab"})
+    console.log(err.message, "vocab no added")
+    return res.status(500).json({ message: "failed to add vocab"})
 
   }
 });
 
-// // test route for getting by user words //accidently added room data, but something to think about for vocab card
-// router.get('/test/:id', async (req,res) => {
+// // gets all vocab for user
+// router.get('/:id', async (req,res) => {
 
 //   try{
-//     const user = await User.findById(req.params.id)
+
+//     const user = await User.findById(req.user._id)
 //                            .populate('vocab.item');
 
 //     console.log("Grabbing user's learned vocab")
@@ -96,5 +78,23 @@ router.get('/:id', async (req,res) => {
 
 //   }
 // });
+
+// // test route for getting by user words //accidently added room data, but something to think about for vocab card
+router.get('/test/:id', async (req,res) => {
+
+  try{
+    const user = await User.findById(req.params.id)
+                           .populate('vocab.item');
+
+    console.log("Grabbing user's learned vocab")
+    return res.json(user.vocab)
+
+  } catch(err) {
+
+    console.log(err.message, "vocab not found")
+    return res.status(500).json({ message: "failed to find vocab"})
+
+  }
+});
 
 export default router
