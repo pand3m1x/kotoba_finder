16933d497d9f03375a5d09606034caad4afa818b
TheRedQueen <pand3m1x@gmail.com>
2026-04-02 21:20:26 -0400

    by grace of the coding gods, connecting the register page was the smoothest and simplest thing I did all day

--- frontend/kotoba-finder/src/pages/Register.jsx
+++ frontend/kotoba-finder/src/pages/Register.jsx
@@ -1,76 +1,80 @@
-// import { useState } from "react";
-// import { userClient } from "../clients/api"
-// import { useUser } from "../context/UserContext"
-// import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { userClient } from "../clients/api"
+import { useUser } from "../context/UserContext"
+import { useNavigate } from "react-router-dom";
 
 function Register() {
 
-  // const { setUser } = useUser()
+  const { setUser } = useUser()
 
-  // const navigate = useNavigate()
-  // // const value = useUser()
-  // // console.log (value)
+  const navigate = useNavigate()
+  // const value = useUser()
+  // console.log (value)
 
-  // const [formData, setFormData] = useState({
-  //   username: '',
-  //   email: '',
-  //   password: ''
-  // })
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: ''
+  })
 
-  // const handleChange = (e) =>{
-  //     setFormData({
-  //       ...formData,
-  //       [e.target.name]: e.target.value // [] makes it dynamic instead of listing each input individually
-  //     })
-  // }
+  const handleChange = (e) =>{
+      setFormData({
+        ...formData,
+        [e.target.name]: e.target.value // [] makes it dynamic instead of listing each input individually
+      })
+  }
 
-  // const handleSubmit = async (e) => {
+  const handleSubmit = async (e) => {
 
-  //   e.preventDefault()
-  //   console.log(formData)
+    e.preventDefault()
+    console.log(formData)
 
-  //   // send the form data to our backend
-  //   try {
+    // send the form data to our backend
+    try {
 
-  //     const { data } = await userClient.post('/register', formData) // response.data also works: where the Response has other properties (like the HTTP code, error information etc)
-  //     console.log(data)
+      const { data } = await userClient.post('/register', formData) // response.data also works: where the Response has other properties (like the HTTP code, error information etc)
+      console.log(data)
 
-  //     // take the token and store it locally
+      // take the token and store it locally
   
-  //       localStorage.setItem("token", data.token) //or response.data.token
+        localStorage.setItem("token", data.token) //or response.data.token
         
-  //     // save some user data in our state
+      // save some user data in our state
   
-  //     setUser(data.user)
+      setUser(data.user)
         
-  //     // take the user to a different page
+      // take the user to a different page
 
-  //     navigate("/feed")
+      navigate('/')
 
-  //   } catch (err) {
+    } catch (err) {
 
-  //       console.log(err)
-  //       alert(err.message)
+        console.log(err)
+        alert(err.message)
 
-  //   }
-  // }
+    }
+  }
 
   return(
-    <div>
+    <div className="registerForm" style={{ border:"2px solid green",
+                                        display:"flex",
+                                        flexDirection:"column",
+                                        alignItems:"center",
+                                        padding:"10px" }}>
       <h1>Register</h1>
-      <form> {/*onSubmit={handleSubmit}*/}
+      <form onSubmit={handleSubmit}>
 
         <label htmlFor="username">Username</label>
-        {/* <input value={formData.username} onChange={handleChange} type="text" id="username" name="username" required/> */}
-        <input type="text" id="userName" name="userName" required /><br/>
+        <input value={formData.username} onChange={handleChange} type="text" id="username" name="username" required/><br/>
+        {/* <input type="text" id="userName" name="userName" required /><br/> */}
 
         <label htmlFor="email">Email</label>
-        {/* <input value={formData.email} onChange={handleChange} type="email" id="email" name="email" required /> */}
-        <input type="email" id="email" name="email" required/>
+        <input value={formData.email} onChange={handleChange} type="email" id="email" name="email" required />
+        {/* <input type="email" id="email" name="email" required/> */}
 
         <br/><label htmlFor="password">Password</label>
-        {/* <input value={formData.password} onChange={handleChange} type="password" id="password" name="password" required /> */}
-        <input type="password" id="password" name="password" required/>
+        <input value={formData.password} onChange={handleChange} type="password" id="password" name="password" required />
+        {/* <input type="password" id="password" name="password" required/> */}
         <br/><button type="submit">Register</button>
 
       </form>
